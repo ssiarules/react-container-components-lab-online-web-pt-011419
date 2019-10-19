47f610272917fba264b7e2665bb8eefbd5448dde
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-19 01:44:27 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,16 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends React.Component{
+  constructor(){
+    super()
+
+    this.state = {
+      reviews: []
+      searchTerm: []
+    }
+  }
+  
+  
+}
