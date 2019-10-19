767a824b11b4b74ba6b4a5294db7b8e2fa56525f
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-19 02:17:13 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,4 +1,17 @@
 // Code MovieReviews Here
-function MovieReviews = () => [
+import React from 'react';
+import Lates
+
+const Review = ({headline, display_title, summary_short}) =>{
+  return (
+    <div key={headline} className='review'>
+      <h2>{display_title}</h2>
+      <p>{summary_short}</p>
+  )
+}
+
+ MovieReviews = () => {
+   
+ }
   
-]
+
